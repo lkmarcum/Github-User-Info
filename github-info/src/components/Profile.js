435a1d9bead91c1988188e276d74845cb20f9d72
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-04-16 16:56:38 -0500

    basic initial structure created

---
+++ github-info/src/components/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Profile() {
+  return <h3>Profile Here</h3>;
+}
+
+export default Profile;
---
+++ github-info/src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Search() {
+  return (
+    <form>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Enter a GitHub username"
+      />
+    </form>
+  );
+}
+
+export default Search;
